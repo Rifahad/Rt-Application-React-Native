f4e0673262b03a58ef9f087fb62042bbd157b551
rifahad <rifahadmt202@gmail.com>
2024-06-20 12:12:55 +0530

    Stated initial setups

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar"
+import { Text, View} from "react-native"
+import {Link} from 'expo-router'
+
+export default function app() {
+  return(
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-3xl'>Rtmax</Text>
+      <StatusBar style="auto"/>
+      <Link href="/profile" className="text-blue-500">Go to Profile</Link>
+    </View>
+  )
+}
