198fbe5601b2b39ca377d558e0e44bf8165bd3a5
rifahad <rifahadmt202@gmail.com>
2024-06-20 18:39:45 +0530

    navigation added

--- app/index.jsx
+++ app/index.jsx
@@ -3,11 +3,13 @@ import { Text, View} from "react-native"
 import {Link} from 'expo-router'
 
 export default function app() {
+  
+
   return(
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl'>Rtmax</Text>
+    <View className="flex-1 items-center justify-center gap-3 bg-black text-white">
+      <Text  className="text-6xl text-white font-pblack">Rt maX</Text>
+      <Link href="/home" className='text-3xl text-white font-pthin'>Go to home</Link>
       <StatusBar style="auto"/>
-      <Link href="/profile" className="text-blue-500">Go to Profile</Link>
     </View>
   )
 }
